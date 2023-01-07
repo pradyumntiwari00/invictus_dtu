99a21c6e4b82f1b032893c8f8c878d605af628a1
Shivansh Dhondiyal <shivanshdhondiyal7723@gmail.com>
2023-01-07 07:03:51 +0530

    ChnagesCompiled

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Home from "./components/Home";
 import "locomotive-scroll/dist/locomotive-scroll.css";
 import About from "./components/About";
 import Teams from "./components/Teams/Teams";
+import Highlights from "./components/highlights";
+import Sponser_tab from "./components/Sponser_tab";
 import Footer from "./components/Footer/Footer";
 import Events from "./components/Events/Events";
 const events = ["Blogs", "Hackathon", "Quiz"];
@@ -13,6 +15,8 @@ function App() {
       <div style={{ width: "400px" }}>HI</div>
       {/* <Home /> */}
       <About />
+      <Sponser_tab />
+      <Highlights />
       {/* <Teams /> */}
       <Events event={events[2]} />
       <Footer />
