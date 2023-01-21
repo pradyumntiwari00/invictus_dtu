fc4f661c3b6ecd902f2da35f5f5d110b89e794c3
Pradyumn Tiwari <pradyumntiwari00@gmail.com>
2023-01-21 18:40:52 +0530

    feat:faq and feat:socials

---
+++ src/components/social/Social.js
@@ -0,0 +1,26 @@
+import React from "react";
+import {BsInstagram} from "react-icons/bs";
+import {SlSocialLinkedin} from "react-icons/sl";
+import {TfiFacebook} from "react-icons/tfi";
+import "./social.css";
+const Social=()=>{
+    return(
+        <>
+        <div className="social">
+         <div className="content">
+            <div className="insta">
+            <a href="https://www.instagram.com/invictus_dtu/"> <BsInstagram size={28} color='white'/></a>
+            </div>
+            <div className="linkedin">
+            <a href="https://www.linkedin.com/company/invictus-dtu/">
+            <SlSocialLinkedin size={28} color='white'/></a>
+            </div>
+            <div className="fb"><a href="https://www.google.com/">
+            <TfiFacebook size={28} color='white'/></a>
+            </div>
+         </div>
+        </div>
+        </>
+    )
+}
+export default Social;
